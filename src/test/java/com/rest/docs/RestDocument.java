ab95440ff10f5fa4c43059b97defb20b1884cbf3
kshneo0 <kshneo@gmail.com>
2022-12-10 11:16:17 +0900

    customize document 2

---
+++ src/test/java/com/rest/docs/RestDocument.java
@@ -0,0 +1,54 @@
+package com.rest.docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * fileName : RestDocumentApi
+ * author :  KimSangHoon
+ * date : 2022/12/10
+ */
+public class RestDocument extends TestSupport {
+
+
+    @Test
+    public void sampleRequest() throws Exception {
+        mockMvc.perform(
+                        post("/test/sample")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\n"
+                                        + "  \"email\": \"zobbb.com\",\n"
+                                        + "  \"name\": \"\"\n"
+                                        + "}")
+                )
+                .andExpect(status().isBadRequest())
+                .andDo(
+                        restDocs.document(
+                                responseFields(
+                                        fieldWithPath("message").description("에러 메시지"),
+                                        fieldWithPath("status").description("Http Status Code"),
+                                        fieldWithPath("code").description("Error Code"),
+                                        fieldWithPath("timestamp").description("에러 발생 시간"),
+                                        fieldWithPath("errors").description("Error 값 배열 값"),
+                                        fieldWithPath("errors[0].field").description("문제 있는 필드"),
+                                        fieldWithPath("errors[0].value").description("문제가 있는 값"),
+                                        fieldWithPath("errors[0].reason").description("문재가 있는 이유")
+                                )
+                        )
+                )
+        ;
+    }
+}
---
+++ src/test/java/com/rest/docs/RestDocumentApi.java
@@ -0,0 +1,48 @@
+package com.rest.docs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * fileName : RestDocumentApi
+ * author :  KimSangHoon
+ * date : 2022/12/10
+ */
+@RestController
+@RequestMapping("/test")
+public class RestDocumentApi {
+
+    @PostMapping("/sample")
+    public void sample(@RequestBody @Valid SampleRequest dto){
+
+    }
+
+    public static class SampleRequest {
+
+        @NotEmpty
+        private String name;
+
+        @Email
+        private String email;
+
+        public SampleRequest(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+}
